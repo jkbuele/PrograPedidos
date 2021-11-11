7a958b470d52c542270ef531d01348db06e73710
jkbuele <83775377+jkbuele@users.noreply.github.com>
2021-11-10 21:57:15 -0500

    first commit

---
+++ src/app/models/models.ts
@@ -0,0 +1,16 @@
+export interface Merienda{
+  
+    platoFuerte: string;
+    jugo: string;
+    mesa: 'C'| 'P';
+  
+  }
+  
+  export interface Menu{
+    sopa: string;
+    platoFuerte: string;
+    jugo: string;
+    cubiertos: number;
+    mesa: 'C'| 'P';
+  
+  }
--- src/app/pages/servicios/servicios.component.ts
+++ src/app/pages/servicios/servicios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Menu } from 'src/app/models/models';
 
 @Component({
   selector: 'app-servicios',
@@ -7,8 +8,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServiciosComponent implements OnInit {
 
-  constructor() { }
+  menu5: number = 3 ;
+  almuerzos: string= "ALMUERZOS";
+  enable: boolean = false;
 
-  ngOnInit() {}
+  menus1: number[]= [0, 4, 9, 9];
+
+  sopa: Menu = {
+    sopa: 'Andina',
+    platoFuerte: 'Burritos al pastor, arroz mexicano',
+    jugo: 'piña',
+    cubiertos: 3,
+    mesa: 'C'
+
+  }
+
+  menus: Menu[]=[
+    {
+      sopa: 'Andina',
+      platoFuerte: 'Burritos al pastor, arroz mexicano',
+      jugo: 'piña',
+      cubiertos: 3,
+      mesa: 'C'
+    },
+    {
+      sopa: 'Arveja con verde',
+      platoFuerte: 'Bistec de carne con papas fritas y arroz',
+      jugo: 'limonada',
+      cubiertos: 4,
+      mesa: 'C'
+    },
+    {
+      sopa: 'Locro de zapallo',
+      platoFuerte: 'Sudado de corvina, chifles, arroz',
+      jugo: 'papaya',
+      cubiertos: 1,
+      mesa: 'P'
+    },
+  ]
+
+  constructor() { 
+    console.log("estoy en el constructor");
+    this.getServicios();
+
+    this.menus.forEach( menus => {
+      console.log('la sopa es ->', menus.sopa);
+      
+    })
+    
+    
+
+    
+  }
+
+  ngOnInit() {
+    console.log("se creo la vista");
+    
+  }
+
+  getServicios(){
+    console.log('estos son los menus ->', this.menus);
+  
+  }
 
 }
+
+
